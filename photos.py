1c3f21d80d75711959233e492869bb6da476977c
evan <74866880+syedevan@users.noreply.github.com>
2025-11-01 10:41:02 +0000

    Initial commit: Flask app with MySQL and B2C integration

--- photos.py
+++ photos.py
@@ -1,78 +1,126 @@
 import os
+from flask import Flask, redirect, url_for, request, render_template
 from azure.storage.blob import BlobServiceClient
-from flask import Flask, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
+# --- 1. AZURE CONFIGURATION ---
+# App Name
 app = Flask(__name__)
 
-connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING') # retrieve the connection string from the environment variable
-container_name = "photos" # container name in which images will be store in the storage account
+# Blob Storage Connection
+connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+CONTAINER_NAME = os.getenv("CONTAINER_NAME", "photos")
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+container_client = blob_service_client.get_container_client(CONTAINER_NAME)
 
-blob_service_client = BlobServiceClient.from_connection_string(conn_str=connect_str) # create a blob service client to interact with the storage account
 try:
-    container_client = blob_service_client.get_container_client(container=container_name) # get container client to interact with the container in which images will be stored
-    container_client.get_container_properties() # get properties of the container to force exception to be thrown if container does not exist
-except Exception as e:
-    print(e)
-    print("Creating container...")
-    container_client = blob_service_client.create_container(container_name) # create a container in the storage account if it does not exist
+    container_client.create_container()
+except Exception:
+    # Container likely already exists
+    pass
 
-@app.route("/")
-def view_photos():
-    blob_items = container_client.list_blobs() # list all the blobs in the container
+# --- 2. DATABASE CONFIGURATION ---
+# Database URI is set as an Application Setting in Azure
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-    img_html = "<div style='display: flex; justify-content: space-between; flex-wrap: wrap;'>"
+# --- 3. DATABASE MODEL ---
+class PhotoMetadata(db.Model):
+    # Table to store photo metadata and vote counts
+    id = db.Column(db.Integer, primary_key=True)
+    blob_name = db.Column(db.String(255), unique=True, nullable=False)
+    likes = db.Column(db.Integer, default=0)
+    dislikes = db.Column(db.Integer, default=0)
 
-    for blob in blob_items:
-        blob_client = container_client.get_blob_client(blob=blob.name) # get blob client to interact with the blob and get blob url
-        img_html += "<img src='{}' width='auto' height='200' style='margin: 0.5em 0;'/>".format(blob_client.url) # get the blob url and append it to the html
+    def __repr__(self):
+        return f'<Photo {self.blob_name}>'
+
+# --- 4. APP ROUTES ---
+
+@app.route('/')
+def index():
+    # List all blobs and get their metadata from the DB
     
-    img_html += "</div>"
+    # 1. Get list of blobs from storage
+    blob_list = container_client.list_blobs()
+    
+    # 2. Get all photo metadata from the database
+    metadata_list = PhotoMetadata.query.all()
+    metadata_dict = {m.blob_name: m for m in metadata_list}
 
-    # return the html with the images
-    return """
-    <head>
-    <!-- CSS only -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <div class="container">
-                <a class="navbar-brand" href="/">Photos App</a>
-            </div>
-        </nav>
-        <div class="container">
-            <div class="card" style="margin: 1em 0; padding: 1em 0 0 0; align-items: center;">
-                <h3>Upload new File</h3>
-                <div class="form-group">
-                    <form method="post" action="/upload-photos" 
-                        enctype="multipart/form-data">
-                        <div style="display: flex;">
-                            <input type="file" accept=".png, .jpeg, .jpg, .gif" name="photos" multiple class="form-control" style="margin-right: 1em;">
-                            <input type="submit" class="btn btn-primary">
-                        </div>
-                    </form>
-                </div> 
-            </div>
+    photos_with_metadata = []
+    
+    for blob in blob_list:
+        if blob.name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp')):
+            # Get public URL for the blob
+            blob_client = container_client.get_blob_client(blob)
+            photo_url = blob_client.url
+
+            # Get metadata or use defaults if not in DB yet
+            metadata = metadata_dict.get(blob.name, PhotoMetadata(blob_name=blob.name))
+            
+            photos_with_metadata.append({
+                'name': blob.name,
+                'url': photo_url,
+                'likes': metadata.likes,
+                'dislikes': metadata.dislikes
+            })
+
+    return render_template('index.html', photo_url='/photos/', blob_list=photos_with_metadata)
+
+
+@app.route('/upload_photo', methods=['POST'])
+def upload_photo():
+    file = request.files.get('file_upload')
+
+    if not file or file.filename == '':
+        return redirect(url_for('index'))
+
+    # 1. Upload file to Blob Storage
+    blob_client = container_client.get_blob_client(file.filename)
+    blob_client.upload_blob(file, overwrite=True)
+
+    # 2. Create entry in the database if it doesn't exist (initializes likes/dislikes to 0)
+    metadata = PhotoMetadata.query.filter_by(blob_name=file.filename).first()
+    if not metadata:
+        metadata = PhotoMetadata(blob_name=file.filename)
+        db.session.add(metadata)
+        db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/delete/<blob_name>', methods=['POST'])
+def delete_photo(blob_name):
+    # 1. Delete from Blob Storage
+    blob_client = container_client.get_blob_client(blob_name)
+    try:
+        blob_client.delete_blob()
+    except Exception as e:
+        print(f"Error deleting blob: {e}")
+
+    # 2. Delete from Database
+    metadata = PhotoMetadata.query.filter_by(blob_name=blob_name).first()
+    if metadata:
+        db.session.delete(metadata)
+        db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/vote/<blob_name>/<action>', methods=['POST'])
+def vote(blob_name, action):
+    # Retrieve or create metadata for the photo
+    metadata = PhotoMetadata.query.filter_by(blob_name=blob_name).first()
+    if not metadata:
+        # Should not happen often if upload works, but handles missing metadata
+        metadata = PhotoMetadata(blob_name=blob_name)
+        db.session.add(metadata)
         
-    """ + img_html + "</div></body>"
-
-#flask endpoint to upload a photo
-@app.route("/upload-photos", methods=["POST"])
-def upload_photos():
-    filenames = ""
-
-    for file in request.files.getlist("photos"):
-        try:
-            container_client.upload_blob(file.filename, file) # upload the file to the container using the filename as the blob name
-            filenames += file.filename + "<br /> "
-        except Exception as e:
-            print(e)
-            print("Ignoring duplicate filenames") # ignore duplicate filenames
-        
-    return redirect('/')     
-
-# TODO: Make tokens accessible only by SAS
-# https://docs.microsoft.com/en-us/python/api/azure-storage-blob/azure.storage.blob?view=azure-python#generate-blob-sas-account-name--container-name--blob-name--snapshot-none--account-key-none--user-delegation-key-none--permission-none--expiry-none--start-none--policy-id-none--ip-none----kwargs-
-# https://stackoverflow.com/questions/62523166/how-can-i-generate-an-azure-blob-sas-url-in-python
-# https://www.py4u.net/discuss/252694
-# https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/storage/azure-storage-blob/samples/blob_samples_authentication.py#L110
+    if action == 'like':
+        metadata.likes += 1
+    elif action == 'dislike':
+        metadata.dislikes += 1
+    
+    db.session.commit()
+    return redirect(url_for('index'))
